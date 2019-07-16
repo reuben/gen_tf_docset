f48eb0fc6dcdf42605fca058bf3ac16a40c71a0b
Reuben Morais <reuben.morais@gmail.com>
2019-07-16 18:16:51 +0200

    Handle case sensitive conflicts by renaming conflicting files

--- gen.py
+++ gen.py
@@ -1,4 +1,5 @@
 import argparse
+import collections
 import fnmatch
 import misaka
 import os
@@ -29,6 +30,9 @@ def main(markdown_root, html_root):
     renderer = misaka.Markdown(HighlighterRenderer(flags=('hard-wrap',)),
         extensions=('fenced-code', 'no-intra-emphasis', 'tables', 'autolink', 'space-headers', 'strikethrough', 'superscript'))
 
+    # converted filename -> number of (case sensitive) conflicts
+    case_sensitive_conflicts = collections.defaultdict(int)
+
     root_len = len(markdown_root)
     for root, dirnames, filenames in os.walk(markdown_root):
         for filename in fnmatch.filter(filenames, '*.md'):
@@ -36,12 +40,26 @@ def main(markdown_root, html_root):
             out_file = os.path.splitext(md_file)[0] + '.html'
 
             # Remove markdown root from path and append to HTML output root
-            out_file = os.path.join(html_root, out_file[root_len:])
+            # Also strip path separators since
+            #   os.path.join('foo', '/bar') == '/bar'
+            out_file = os.path.join(html_root, out_file[root_len:].strip(os.sep))
 
             # If destination folder does not exist, create it
             out_dir = os.path.dirname(out_file)
             if not os.path.exists(out_dir):
                 os.makedirs(os.path.dirname(out_file))
+
+            # If the same file name (case insensitive) has already been
+            # written to this folder, add a suffix to make it different to
+            # avoid problems in case insensitive filesystems
+            if case_sensitive_conflicts[out_file.lower()] > 0:
+                start, ext = os.path.splitext(out_file)
+                out_file = '{start}_{num}{ext}'.format(
+                    start=start,
+                    num=case_sensitive_conflicts[out_file.lower()],
+                    ext=ext)
+
+            case_sensitive_conflicts[out_file.lower()] += 1
 
             # Render Markdown and write it
             with open(md_file, 'r') as fin, open(out_file, 'w') as fout:
